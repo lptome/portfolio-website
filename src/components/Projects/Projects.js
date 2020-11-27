762fc9f8ec589efeec8c34e793b3123c45367260
Luis Tome <luisptcolaco@gmail.com>
2020-11-27 22:33:40 +0000

    Refactor Projects.js to display through map()

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,74 +23,25 @@ function Projects() {
       transition={PageTransitions.portfolio}
       className="projects"
     >
-      <motion.div
-        variants={ComeDown}
-        transition={PageTransitions.portfolio}
-        className="card-container"
-      >
-        <div className="row project-card">
-          <div className="col-md-12 project-title">
-            <h1>{item[0].title}</h1>
+      {item.map((item) => (
+        <motion.div
+          variants={ComeDown}
+          transition={PageTransitions.portfolio}
+          className="card-container"
+        >
+          <div className="row project-card">
+            <div className="col-md-12 project-title">
+              <h1>{item.title}</h1>
+            </div>
+            <CardImage item={item} />
+            <CardInfo item={item} />
+            <CardTech item={item} />
           </div>
-          <CardImage item={item[0]} />
-          <CardInfo item={item[0]} />
-          <CardTech item={item[0]} />
-        </div>
-        <div className="row">
-          <hr className="separator"></hr>
-        </div>
-      </motion.div>
-      <motion.div
-        variants={ComeDown}
-        transition={PageTransitions.portfolio}
-        className="card-container"
-      >
-        <div className="row project-card">
-          <div className="col-md-12 project-title">
-            <h1>{item[1].title}</h1>
+          <div className="row">
+            <hr className="separator"></hr>
           </div>
-          <CardImage item={item[1]} />
-          <CardInfo item={item[1]} />
-          <CardTech item={item[1]} />
-        </div>
-        <div className="row">
-          <hr className="separator"></hr>
-        </div>
-      </motion.div>
-      <motion.div
-        variants={ComeDown}
-        transition={PageTransitions.portfolio}
-        className="card-container"
-      >
-        <div className="row project-card">
-          <div className="col-md-12 project-title">
-            <h1>{item[2].title}</h1>
-          </div>
-          <CardImage item={item[2]} />
-          <CardInfo item={item[2]} />
-          <CardTech item={item[2]} />
-        </div>
-        <div className="row">
-          <hr className="separator"></hr>
-        </div>
-      </motion.div>
-      <motion.div
-        variants={ComeDown}
-        transition={PageTransitions.portfolio}
-        className="card-container"
-      >
-        <div className="row project-card">
-          <div className="col-md-12 project-title">
-            <h1>{item[3].title}</h1>
-          </div>
-          <CardImage item={item[3]} />
-          <CardInfo item={item[3]} />
-          <CardTech item={item[3]} />
-        </div>
-        <div className="row">
-          <hr className="separator"></hr>
-        </div>
-      </motion.div>
+        </motion.div>
+      ))}
     </motion.div>
   );
 }
